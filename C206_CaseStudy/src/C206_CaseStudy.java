3c44303eccbe1f4f5472bdd331e778917ee2221e
20010754-Pang-ShengHui <20010754@DESKTOP-T2HCRHP>
2021-08-04 12:03:14 +0800

    Added email object into both input and register.

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -10,9 +10,10 @@ public class C206_CaseStudy {
 		while (choice!=3) {
 			if (choice==1) {
 				String username = Helper.readString("Username > ");
+				String email = Helper.readString("Email Address > ");
 				String Pw = Helper.readString("New Password > ");
 				String cPw = Helper.readString("Confirm Password > ");
-				register(username,Pw,cPw);
+				register(username,email,Pw,cPw);
 			} else if (choice==2) {
 				member();
 			} else if (choice>3 || choice<1) {
@@ -34,9 +35,10 @@ public class C206_CaseStudy {
 		System.out.println(output);
 	}
 
-	private static boolean register(String username, String Pw, String cpw) {
+	private static boolean register(String username,String email, String Pw, String cpw) {
 		boolean success = false;
 		if (Pw.equals(cpw)) {
+			
 			System.out.println("Registeration Successful!");
 			success = true;
 		} else {
