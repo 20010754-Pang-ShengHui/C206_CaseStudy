7476ef19b498341aa84d4a3a5a1013fab6e19c0f
s <s@DESKTOP-FDI9ST1>
2021-08-05 18:00:23 +0800

    Push addQuotation

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,6 +6,9 @@ public class C206_CaseStudy {
 	private static ArrayList<Account> accList = new ArrayList<>();
 	private static String loginUser;
 	private static String loginPassword;
+	
+	private static ArrayList<Quotation> quoteList = new ArrayList<>();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		start();
@@ -129,7 +132,7 @@ public class C206_CaseStudy {
 		System.out.println(output);
 	}
 	
-	public void addQuotation() {
+	public void addQuotation(ArrayList<Quotation> quoteList) {
 		
 		int reqid = Helper.readInt("Enter Request ID: ");
 		int quotid = Helper.readInt("Enter Quotation ID:");
@@ -140,10 +143,16 @@ public class C206_CaseStudy {
 		String startdate = Helper.readString("Enter Start Date(mm/dd/yyyy): ");
 		double totalamt = Helper.readDouble("Enter Total Amount: $");
 		
-		ArrayList<Quotation> quoteList = new ArrayList<Quotation>();
-		
 		Quotation quote = new Quotation(reqid, quotid, categ, descrip, price, desname, startdate, totalamt);
 		quoteList.add(quote);
+		
+	}
+	
+	public void viewQuotations(ArrayList<Quotation> quoteList) {
+		
+		System.out.println("VIEW ALL QUOTATIONS");
+		System.out.println(String.format("%-10s %-30s %-10s %-10s %-20s\n", "REQUEST ID", "",
+				"", "",""));
 		
 	}
 	
