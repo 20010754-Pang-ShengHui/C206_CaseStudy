135c90cc0335543249a44f9045b9c9df017a3c30
s <s@DESKTOP-FDI9ST1>
2021-08-06 14:17:05 +0800

    q

--- C206_CaseStudy/src/QuotationMain.java
+++ C206_CaseStudy/src/QuotationMain.java
@@ -17,101 +17,121 @@ public class QuotationMain {
 		output += "3. Delete Quotation\n";
 		output += "4. Quit";
 		System.out.println(output);
+		
 		int choice = Helper.readInt("Enter choice > ");
 		while (choice!=4) {
-		}
-		if (choice==4) {
-			System.out.println("Bye!");
-			System.exit(0);
-		}
+			
+			if (choice==1) {
+				
+				int reqid = Helper.readInt("Enter Request ID: ");
+		        int quotid = Helper.readInt("Enter Quotation ID:");
+		        String categ = Helper.readString("Enter Renovation Category: ");
+		        String descrip = Helper.readString("Enter Description: ");
+		        double price = Helper.readDouble("Enter Item Price: ");
+		        String desname = Helper.readString("Enter Designer Name: ");
+		        String startdate = Helper.readString("Enter Start Date(mm/dd/yyyy): ");
+		        double totalamt = Helper.readDouble("Enter Total Amount: $");
 		
+		        Quotation q1 = new Quotation(reqid, quotid, categ, descrip, price, desname, startdate, totalamt);
+		        addQuotation(quoteList, q1);
+		        
+			} else if (choice == 2) {
+				viewQuotations(quoteList);
+				
+			} else if (choice == 3) {
+				deleteQuotation();
+			
+			
+			
+		    } else if (choice==4) {
+			    System.out.println("Bye!");
+			    System.exit(0);
+		    }
+		} 
 	}
 	
     public static void addQuotation(ArrayList<Quotation> quoteList, Quotation q1) {
 		
-		int reqid = Helper.readInt("Enter Request ID: ");
-		int quotid = Helper.readInt("Enter Quotation ID:");
-		String categ = Helper.readString("Enter Renovation Category: ");
-		String descrip = Helper.readString("Enter Description: ");
-		double price = Helper.readDouble("Enter Item Price: ");
-		String desname = Helper.readString("Enter Designer Name: ");
-		String startdate = Helper.readString("Enter Start Date(mm/dd/yyyy): ");
-		double totalamt = Helper.readDouble("Enter Total Amount: $");
+	    quoteList.add(q1);
+	    System.out.println("Quotation Added!");
 		
-		if (categ.isEmpty() || descrip.isEmpty() ) {
-			System.out.println("Please fill in all the neccessary field");
-			
-		} else {
-		
-		    Quotation quote = new Quotation(reqid, quotid, categ, descrip, price, desname, startdate, totalamt);
-		    quoteList.add(quote);
-		}
 		
 	}
 	
-	public static String viewQuotations(ArrayList<Quotation> quoteList) {
-		String output = "";
+	public static void viewQuotations(ArrayList<Quotation> quoteList) {
+		
 		
 		if (quoteList.isEmpty()) {
 			
 			Helper.line(70, "-");
-			output += "There are no Quotations currently."; 
+			System.out.println("There are no Quotations currently."); 
 		  
 		} else {
 			 System.out.println("VIEW ALL QUOTATIONS");
-		    System.out.println(String.format("%-10s %-20s %-15s %-15s %-15s %-15s %-15s %-15s\n", "REQUEST ID", 
+		     System.out.println(String.format("%-10s %-20s %-15s %-15s %-15s %-15s %-15s %-15s\n", "REQUEST ID", 
 				"QUOTATION ID", "CATEGORY", "DESCRIPTION", "PRICE", "DESIGNER NAME", "START DATE", 
 				"TOTAL AMOUNT"));
 		
 		    for (int i = 0; i < quoteList.size(); i++) {
 			
 			    Quotation q = quoteList.get(i);
-			    output += String.format("%-10d %-15d %-15s %-15s %-15.2f %-15s %-15s %-15.2f\n", q.getReqid(), 
+			    System.out.println(String.format("%-10d %-15d %-15s %-15s %-15.2f %-15s %-15s %-15.2f\n", q.getReqid(), 
 					q.getQuotid(), q.getCateg(), q.getDescrip(), q.getPrice(), q.getDesname(), 
-					q.getStartdate(), q.getTotalamt());
+					q.getStartdate(), q.getTotalamt()));
 			
 		    }
 		}
-		return output;
+		
 	}
 	
+	public static void DelQuo(ArrayList<Quotation> quoteList, Quotation q) {
+		for (int i = 0;i< quoteList.size(); i++) {
+			if (quoteList.get(i) == q) {
+				quoteList.remove(q);
+			}
+			
+		}
+	}
 	
-	
-	
-	
-	public static int deleteQuotation(ArrayList<Quotation> quoteList, int qu) {
+	public static void deleteQuotation() {
+		
 		
 		if (!quoteList.isEmpty()) {
+			viewQuotations(quoteList);
 			
-			int delquote = Helper.readInt("Enter a Quotation ID to delete: ");
-		
-		    
-		    char yn = Helper.readChar("Are you sure you want to delete? (Y/N) > ");
+			int delquote = Helper.readInt("Enter Quotaion ID to delete: ");
+		    char yn = Helper.readChar("Are you sure you want to delete? (Y/N): ");
 		    
 		    if (yn=='Y' || yn=='y') {
 		        for (int i = 0; i < quoteList.size(); i++) {
 			
 			        Quotation q = quoteList.get(i);
-			        if ( delquote == q.getQuotid()) {       	
-			 
-				        quoteList.remove(q);
+			        if ( delquote == q.getQuotid()) {       
+			        	
+			        	DelQuo(quoteList, q);
+				   
 				        System.out.println("Your Quotation is successfully deleted!");
 				        break;
 				        
-			        } else {
-			        	System.out.println("Quotation ID not found!");
-			        }
+			        } 
 		
-	            }
+	            } 
 		        
 		    } else if (yn=='N' || yn=='n') {
 				System.out.println("Deletion canceled!");
+				System.out.println("Going back...");
+				try {
+		            Thread.sleep(500);
+		        } catch (Exception e) {
+		            System.out.println(e);
+		        }
 		    }
+		    
 		} else {
 			Helper.line(70, "-");
 			System.out.println("There are no Quotations currently.");
 		}
-		return qu;
+		
 	}
 
 }
--- C206_CaseStudy/test/QuotationMainTest.java
+++ C206_CaseStudy/test/QuotationMainTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*; 
+import static org.junit.Assert.*;  
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -54,57 +54,46 @@ public class QuotationMainTest {
 		assertEquals("Test that Quotation arraylist size is 2", 2, quoteListTest.size());
 
 		
-		// test that account is deleted
-		int quoid = q1.getQuotid();
-		int delquote = QuotationMain.deleteQuotation(quoteListTest, quoid);
-		assertEquals("Test that account arraylist size is 1", 1, quoteListTest.size());
-
+		QuotationMain.DelQuo(quoteListTest, q1);
+		assertEquals("Check whether the Quotation List is 1", 1, quoteListTest.size());
+		assertNotSame("Check whether q1 is added", q1, quoteListTest.get(0));
+	
 		
-		// test that the successful message displayed
-		String delMsgTest = q1.getQuotid() + " has been successfully deleted";
-		assertEquals("Test that successful message displayed", delquote, delMsgTest);
-
-		
-		// test that account deleted is the correct one
-		String delMsgErrorTest = "No user with that name is found";
-		delquote = QuotationMain.deleteQuotation(quoteListTest, quoid);
-		assertSame("Test that Quotation deleted is Quotation 1", delquote, delMsgErrorTest);
+		QuotationMain.DelQuo(quoteListTest, q2);
+		assertEquals("Check whether the Appointment List is 1", 0, quoteListTest.size());
 
 	}
 	
 	@Test
-	public void viewQuotations() {
-		
-		// Quotation list is not null, so that can add a new Quotation - boundary
-		assertNotNull("Check if there is valid Quotation arraylist to add to", quoteListTest);
-		
-		
-		// Test if the list of Quotation retrieved from the database is empty - boundary
-		String allQuote = QuotationMain.viewQuotations(quoteListTest);
-		String testOutput = "No users found\n";
-		assertSame("Check ViewAllquotationlist", testOutput, allQuote);
-		
-		
-		// Given an empty list, after adding 2 items, test if the size of the list is 2 - normal
-		QuotationMain.addQuotation(quoteListTest, q1);
-		QuotationMain.addQuotation(quoteListTest, q2);
-		assertEquals("Test that account arraylist size is 2", 2, quoteListTest.size());
-		
-		
-		// Test if the expected output string same as the list of account retrieved from the database
-		allQuote = QuotationMain.viewQuotations(quoteListTest);
-		testOutput = String.format("%-10d %-15d %-15s %-15s %-15.2f %-15s %-15s %-15.2f\n", 23, 45, "Kitchen", "Tiles", 250.00, "ven", "08/25/2021", 250.00);
-		testOutput += String.format("%-10d %-15d %-15s %-15s %-15.2f %-15s %-15s %-15.2f\n", 58, 98, "Bedroom", "Cabinet", 340.00, "umi", "09/10/2021", 340.00);
-		assertEquals("Test ViewAllQuotationsList", testOutput, allQuote);
-		
-		
-	}
+//	public void viewQuotations() {
+//		
+//		// Quotation list is not null, so that can add a new Quotation - boundary
+//		assertNotNull("Check if there is valid Quotation arraylist to add to", quoteListTest);
+//		
+//		
+//		// Test if the list of Quotation retrieved from the database is empty - boundary
+//		String allQuote = QuotationMain.viewQuotations(quoteListTest);
+//		String testOutput = "No users found\n";
+//		assertSame("Check ViewAllquotationlist", testOutput, allQuote);
+//		
+//		
+//		// Given an empty list, after adding 2 items, test if the size of the list is 2 - normal
+//		QuotationMain.addQuotation(quoteListTest, q1);
+//		QuotationMain.addQuotation(quoteListTest, q2);
+//		assertEquals("Test that account arraylist size is 2", 2, quoteListTest.size());
+//		
+//		
+//		// Test if the expected output string same as the list of account retrieved from the database
+//		allQuote = QuotationMain.viewQuotations(quoteListTest);
+//		testOutput = String.format("%-10d %-15d %-15s %-15s %-15.2f %-15s %-15s %-15.2f\n", 23, 45, "Kitchen", "Tiles", 250.00, "ven", "08/25/2021", 250.00);
+//		testOutput += String.format("%-10d %-15d %-15s %-15s %-15.2f %-15s %-15s %-15.2f\n", 58, 98, "Bedroom", "Cabinet", 340.00, "umi", "09/10/2021", 340.00);
+//		assertEquals("Test ViewAllQuotationsList", testOutput, allQuote);
+//		
+//	}
 	
 	@After
 	public void tearDown() throws Exception {
 		
-		q1 = null;
-		q2 = null;
-		quoteListTest = null;
+		
 	}
 }
