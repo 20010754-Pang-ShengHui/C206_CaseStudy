944b090e3d1535c323c207a48fe79a9d447b4622
s <s@DESKTOP-FDI9ST1>
2021-08-06 09:51:34 +0800

    .

--- C206_CaseStudy/src/QuotationMain.java
+++ C206_CaseStudy/src/QuotationMain.java
@@ -85,6 +85,8 @@ public class QuotationMain {
 			        if (delquote == q.getQuotid()) {
 				        quoteList.remove(q);
 				        System.out.println("Your Quotation is successfully deleted!");
+				        break;
+				        
 			        } else {
 			        	System.out.println("Quotation ID not found!");
 			        }
